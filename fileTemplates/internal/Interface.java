a886063f2e070e01ec12ea0644341fe4a44d7d9f
tiancai.zang <laidu823@sina.com>
2018-02-27 12:26:39 +0800

    IU-2017.3.4 <laidu@laidudemacbook-pro.local Create ide.general.xml, project.default.xml, Enum.java, Interface.java, git.xml, terminal.xml, Default.xml, file.template.settings.xml, ignore.xml, find.xml, ui.lnf.xml, lombok.xml, module-info.java, package-info.java, plugin_ui.xml, colors.scheme.xml, java.xml, path.macros.xml, databaseDrivers.xml, github_settings.xml, templates.xml, AnnotationType.java, Class.java, jdk.table.xml, laf.xml, proxy.settings.xml, vcs.xml, editor.codeinsight.xml, grepConsole.xml, databaseSettings.xml, keymap.xml, editor.xml, debugger.xml

---
+++ fileTemplates/internal/Interface.java
@@ -0,0 +1,9 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+/**
+ * ${Description}
+ *
+ * Created by tiancai.zang 
+ * on ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}.
+ */
+public interface ${NAME} {
+}
---
+++ fileTemplates/internal/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * ${Description}
+ *
+ * Created by tiancai.zang 
+ * on ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}.
+ */
+module #[[$MODULE_NAME$]]# {
+}
