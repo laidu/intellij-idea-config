58964c159499389fa7c321dc1b8209ba13677122
laidu <laidu823@gmail.com>
2018-04-08 23:50:05 +0800

    IU-2018.1 <laidu@localhost Overwrite remote git@github.com:laidu/intellij-idea-config.git to local

--- fileTemplates/internal/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * ${Description}
- *
- * Created by tiancai.zang 
- * on ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}.
- */
-#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
